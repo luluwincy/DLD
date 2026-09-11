6efd40fee8a52bba0b66829915335fa1cdb90a73
luluwincy <luluwincy@gmail.com>
2026-09-11 18:13:26 -0400

    add and, not, or gates

---
+++ or_gate/or_gate_tb.sv
@@ -0,0 +1,38 @@
+`timescale 1ns/1ps
+
+module or_gate_tb;
+
+reg a;
+reg b;
+wire y;
+
+or_gate newOr (
+    .a(a),
+    .b(b),
+    .y(y)
+);
+
+initial begin
+    $dumpfile("or_gate.vcd");
+    $dumpvars(0, or_gate_tb);
+    
+    a = 0;
+    b = 0;
+    #10;
+
+    a = 0;
+    b = 1;
+    #10;
+
+    a = 1;
+    b = 0;
+    #10;
+
+    a = 1;
+    b = 1;
+    #10;
+
+    $finish;
+end
+
+endmodule
